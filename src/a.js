9ac0de8bfeae9ef9740b8a78b05861fbafd4d47c
Vladimir Kriukov <zeoniss28@gmail.com>
2021-01-22 23:16:39 +0200

    summary

---
+++ src/a.js
@@ -0,0 +1,4 @@
+import element from './b';
+console.log(element);
+const a = 5;
+export default a;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import value from "./a";
+console.log(value);
+const add = (a, b) => a + b;
+add(2, 3);
